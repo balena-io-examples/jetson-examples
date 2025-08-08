cf8108965cb471a9cd501b64959b75efa76da656
Alexandru Costache <alexandru@balena.io>
2025-08-08 09:27:57 +0000

    Re-work examples to drop balenalib usage
    Since base images are no longer provided,
    we switch to using upstream Docker base images
    on top of which we enable udev.
    
    Change-type: minor
    Author: Alan Boris <alan@balena.io>
    Signed-off-by: Alan Boris <alan@balena.io>
    Signed-off-by: Alexandru Costache <alexandru@balena.io>

---
+++ jetson-orin/startxfce.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Ensure plymouth exited so that it won't hold /dev/fb0 open and tegra_drm can be unloaded
+DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket \
+  dbus-send \
+  --system \
+  --print-reply \
+  --dest=org.freedesktop.systemd1 \
+  /org/freedesktop/systemd1 \
+  org.freedesktop.systemd1.Manager.StopUnit \
+  string:plymouth-quit.service string:replace
+
+# Prevent "Server is already active for display 0" error,
+# in case X was forcedly closed before
+rm -rf /tmp/.X0-lock* || true
+
+# Prevent black screen with cursor only
+rm -rf /root/.config/ || true
+
+modules=("tegra_drm" "nvidia_drm" "nvidia_modeset"); for module in "${modules[@]}";
+do
+    if lsmod | grep -q ${module} ; then
+        echo "Found $module"; rmmod $module;
+    fi;
+done;
+
+startxfce4 & sleep 5;
+
+modprobe tegra_drm;
+modprobe nvidia_drm;
+while [ 1 ]; do
+   sleep 10;
+done;
